7c79974b4e34540e9991c39edf16303eccd52ac7
권현준 <bonheur9813@naver.com>
2024-08-26 13:55:05 +0900

    test webhook push event

--- react-test/src/App.jsx
+++ react-test/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>리액트 Github Action 연습</h1>
+      <h1>Webhook 되려나..?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
